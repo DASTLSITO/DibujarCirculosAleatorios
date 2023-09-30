ac7853b10c558df02feff11bbb2e3b5bd6936e69
DASTLSITO <dastlhelp@gmail.com>
2023-09-29 17:17:03 -0700

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace P222310540TM
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            panel1 = new Panel();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(24, 111, 101);
+            button1.FlatStyle = FlatStyle.Popup;
+            button1.Location = new Point(12, 465);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 47);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(252, 224, 155);
+            panel1.Location = new Point(130, 12);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(500, 500);
+            panel1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(181, 203, 153);
+            ClientSize = new Size(641, 521);
+            Controls.Add(panel1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private Panel panel1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,102 @@
+using System.Reflection.Metadata.Ecma335;
+using System.Windows.Forms;
+
+namespace P222310540TM
+{
+    public partial class Form1 : Form
+    {
+        int centroX = 0;
+        int centroY = 0;
+        Graphics g;
+
+        byte circulos = 0;
+
+        double angulo = 0;
+
+        int vecesLlamado = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            centroX = panel1.Width / 2;
+            centroY = panel1.Height / 2;
+
+            g = panel1.CreateGraphics();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            PintarCirculoPadre();
+        }
+
+        protected void PintarCirculoPadre()
+        {
+            int radioGrande = 240;
+            g.DrawEllipse(Pens.Black, centroX - radioGrande, centroY - radioGrande, radioGrande * 2, radioGrande * 2);
+
+            angulo = Math.PI * 2 / circulos;
+            PintarCirculosHijosMasChingona(radioGrande, centroX, centroY, vecesLlamado);
+        }
+
+        public void PintarCirculosHijos(float radioGrande, float centroX, float centroY, int nivel)
+        {
+            //Acaba el metodo si ya no hay veces
+            if (nivel != 0)
+            {
+                float hipotenusa = 0;
+                float radioChiquito = 0;
+                float cateto = 0;
+
+                float punto1x = 0;
+                float punto2x = 0;
+                float punto3x = 0;
+
+                float punto1y = 0;
+                float punto2y = 0;
+                float punto3y = 0;
+
+                radioChiquito = (float)(radioGrande * Math.Sin((Math.PI) / 3) / (Math.Sin((Math.PI) / 3) + 1));
+                hipotenusa = (float)(radioChiquito / Math.Sin((Math.PI) / 3));
+                cateto = (float)((radioGrande * Math.Cos((Math.PI) / 3)) / (Math.Sin((Math.PI) / 3) + 1));
+
+                punto1x = centroX - radioChiquito;
+                punto1y = centroY - cateto;
+
+                punto2x = centroX + radioChiquito;
+                punto2y = centroY - cateto;
+
+                punto3x = centroX;
+                punto3y = centroY + hipotenusa;
+
+                g.DrawEllipse(Pens.Black, punto1x - radioChiquito, punto1y - radioChiquito, radioChiquito * 2, radioChiquito * 2);
+                g.DrawEllipse(Pens.Black, punto2x - radioChiquito, punto2y - radioChiquito, radioChiquito * 2, radioChiquito * 2);
+                g.DrawEllipse(Pens.Black, punto3x - radioChiquito, punto3y - radioChiquito, radioChiquito * 2, radioChiquito * 2);
+
+                PintarCirculosHijos(radioChiquito, punto1x, punto1y, nivel - 1);
+                PintarCirculosHijos(radioChiquito, punto2x, punto2y, nivel - 1);
+                PintarCirculosHijos(radioChiquito, punto3x, punto3y, nivel - 1);
+            }
+        }
+
+        public void PintarCirculosHijosMasChingona(float radioGrande, float centroX, float centroY, int nivel)
+        {
+            if (nivel == 0)
+                return;
+
+            float hipotenusa = 0;
+            float radioChiquito = 0;
+
+            PointF[] puntos = new PointF[circulos];
+
+            radioChiquito = (float)(radioGrande * Math.Sin(angulo / 2) / (Math.Sin(angulo / 2) + 1));
+            hipotenusa = (float)(radioChiquito / Math.Sin(angulo / 2));
+
+            for (int i = 0; i < circulos; i++)
+            {
+                puntos[i] = new PointF((float)(centroX + hipotenusa * Math.Cos(Math.PI + angulo * i)), (float)(centroY + hipotenusa * Math.Sin(Math.PI + angulo * i)));
+                g.DrawEllipse(Pens.Black, puntos[i].X - radioChiquito, puntos[i].Y - radioChiquito, radioChiquito * 2, radioChiquito * 2);
+                PintarCirculosHijosMasChingona(radioChiquito, puntos[i].X, puntos[i].Y, nivel - 1);
+            }
+        }
+    }
+}
